7f8adc9bb14d0e51157f303702ad475b013c2f8e
Claud <claud.xiao@outlook.com>
2022-02-25 23:58:37 +0800

    Further Done

--- main.py
+++ main.py
@@ -1,4 +1,52 @@
 # Suppose up/down limitation (1) & [7]
+def demapTone(ta):
+    ret = ""
+    if ta == -1:
+        ret = "\n"
+        return ret
+    absT = ta%12
+    if absT <5:
+        ret = "%d"%(int(absT/2) + 1)
+        if absT%2 == 1:
+            ret = "#" + ret
+    else:
+        ret = "%d"%(int((absT+1)/2) + 1)
+        if absT%2 == 0:
+            ret = "#" + ret
+    if ta <= 11:
+        ret = "("+ret+")"
+    elif ta > 23:
+        ret = "[" + ret + "]"
+    return ret
+
+def mapTone(ta):
+    ret = 0
+    #map Tone to real number steps
+    # (1) = 0
+    # [#7] = 30
+    import re
+    pattern = re.compile(r'(.*)(\d)(.*)')
+    # get dig
+    try:
+        dta = int(pattern.match(ta).group(2))
+        # Number
+        if dta < 4:
+            ret = (dta-1) * 2
+        else:
+            ret = (dta-4)*2 + 5
+        #  [/)
+        if re.match(r'.*\[.*', ta) is not None:
+            ret = ret + 24
+        elif re.match(r'.*\(.*', ta) is not None:
+            ret = ret
+        else:
+            ret = ret + 12
+        # \#
+        if re.match(r'.*#.*', ta) is not None:
+            ret = ret + 1
+    except:
+        ret = -1
+    return ret
 def compTone(ta,tb):
     import re
     pattern = re.compile(r'(.*)(\d)(.*)')
@@ -24,6 +72,7 @@ def compTone(ta,tb):
 
 def fetchInput(fpath):
     # 打开文件
+    toneNArr = []
     toneArr = []
     fo = open(fpath, "r+")
     print("Read File: ", fo.name)
@@ -61,17 +110,22 @@ def fetchInput(fpath):
         # Do nothing
             pass
         toneArr.append(pt)
-    print(toneArr)
-    print("Highest Tone is: " + upL)
-    print("Lowest Tone is: " + downL)
-    # Todo: Analyze the space for adjustment based on up/down limitation
-    # Todo: Provide Suggestion on adjustment further
-
+        toneNArr.append(mapTone(pt))
+    # Analyze the space for adjustment based on up/down limitation
+    leftS = 0-mapTone(downL)
+    rightS = 35-mapTone(upL)
     # 关闭文件
     fo.close()
-
+    return [toneArr, toneNArr,[leftS,rightS]]
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # Load note file
-    fetchInput("input.txt")
+    t = fetchInput("input.txt")
+    name = input("Tell me how many steps to shift (%d ~ %d) :"%(t[2][0],t[2][1]))
+    # ToDo: To handle the shifted number list!
+
+
+
+#print(demapTone(mapTone("(#5)")))
+   # print(mapTone("#5"))
